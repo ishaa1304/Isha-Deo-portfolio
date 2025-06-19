0bcdb4fe6f2fd3d139701752ad75a18a37c43cf6
Isha Deo <76875905+ishaa1304@users.noreply.github.com>
2025-06-19 17:16:21 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ export const HeroSection = () => {
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
             <span className="opacity-0 animate-fade-in">Hi I am </span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">
-              Isha Deo
+              Isha Deo !
             </span>
           </h1>
 
